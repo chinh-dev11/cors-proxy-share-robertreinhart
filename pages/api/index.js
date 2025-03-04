7f0f688e8fceae4d821579cf93da265b87bda85d
Chinh <chinh.dev@icloud.com>
2025-03-04 13:35:36 -0500

    test cors

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,20 +1,23 @@
-const axios = require('axios');
+import axios from "axios";
 
 export default function handler(req, res, next) {
   async function getURI(url) {
+    console.log(url, "URL");
     try {
       const response = await axios.get(url);
       if (response.status !== 200) {
-        return res.status(response.status).json({ type: 'error', message: response.statusText });
+        return res
+          .status(response.status)
+          .json({ type: "error", message: response.statusText });
       } else {
-      res.json(JSON.stringify(response.data))
+        res.json(JSON.stringify(response.data));
       }
     } catch (error) {
-      console.log(error.message, "ERR")
+      console.log(error.message, "ERR");
       // return res.status(500).json({ type: 'error', message: error.message });
     }
-  } 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  getURI(JSON.parse(req.body)['my-url'])
+  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Content-Type", "application/json");
+  getURI(JSON.parse(req.body)["my-url"]);
 }
